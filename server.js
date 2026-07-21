1cf1dcc439faf6053dd1551ed39c06ea45d11b25
neeraj-code-beep <neerajmishra757944@gmail.com>
2026-07-21 14:32:47 +0530

    Socket.io setup done

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const app = require('./src/app');
 const port = 3000;
 const ConnectToDB = require('./src/db/db');
+const initSocketServer = require('./src/sockets/socket.server');
+const httpServer = require('http').createServer(app);
 
 ConnectToDB();
+initSocketServer(httpServer);
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
